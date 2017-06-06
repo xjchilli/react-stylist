cc36809b066076a14c988580aea48430560d1113
xiaowang1314 <wangzehu1314@sina.cn>
2017-06-06 18:37:43 +0800

    change

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # webpack2-build
 
 ## 用于react开发  webpack构建使用
+
+## Test 需要安装  jsdom enzyme chai mocha(全局安装)
+
+
+
--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import { render }  from 'react-dom'
-import {Provider} from 'react-redux';
+/*import {
+	render
+} from 'react-dom'
+import {
+	Provider
+} from 'react-redux';
 
 
 import store from './Config/Store';
 import route from './Config/Route';
-import  './Style/dps.less';
+import './Style/dps.less';
 
 render(
-    <Provider store={store}>
+	<Provider store={store}>
         {route}
     </Provider>,
-    document.getElementById('app')
-);
+	document.getElementById('app')
+);*/
 
 
+export default class App extends React.Component {
+	render() {
+		return (
+			<div>
+        <h1>Todos</h1>
+      </div>
+		);
+	}
+}
