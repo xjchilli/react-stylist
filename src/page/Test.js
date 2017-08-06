c0142c9cfff3ee6ff07b353d8469e7c5f05c522a
xiaowang1314 <wangzehu1314@sina.cn>
2017-08-06 19:33:54 +0800

    1

--- src/page/Test.js
+++ src/page/Test.js
@@ -4,8 +4,7 @@
  */
 import React, { Component } from 'react';
 import { ToolDps } from '../ToolDps';
-import flatpickr from 'flatpickr';
-const zh = require("flatpickr/dist/l10n/zh.js").zh;
+import { is, fromJS, Map } from 'immutable';
 
 
 
@@ -13,44 +12,84 @@ class Test extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            date: null
-        }
+            times: 0,
+            date: '1123'
+        };
     }
 
     componentDidMount() {
-        let nowDate = new Date();
-        flatpickr("#date", {
-            locale: zh,
-            minDate: nowDate,
-            disableMobile: "true",
-            enableTime: true,
-            onChange: (selectedDates, dateStr, instance) => {
-                nowDate = selectedDates[0];
-                this.setState({
-                    date: dateStr
-                });
-            },
-            onOpen: () => {
-                this.setState({
-                    date: ToolDps.convertDate(nowDate, 'YYYY-MM-DD hh:mm')
-                });
-            }
+
+    }
+
+   
+
+    modifyTime(e) {
+        this.setState({
+            date:e.target.value
         });
     }
 
-
     render() {
-
         return (
             <div>
                 <p>日期:1</p>
-                {/* <div id='date'>选择日期:{this.state.date}</div> */}
-                <input type="text" id="date" value={this.state.date} placeholder="请选择时间"/>
+                <input value={this.state.date} onChange={this.modifyTime.bind(this)} />
+                <User name={this.state.date}/>
             </div>
         )
     }
 }
 
+class User extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            age:13,
+            name:['potato']
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        
+        this.setState({
+            name:this.state.name.concat(nextProps.name)
+        });
+    }
+    
+    
+     shouldComponentUpdate(nextProps, nextState) {
+        return !is(fromJS(this.props), fromJS(nextProps)) || !is(fromJS(this.state), fromJS(nextState))
+        // const thisProps = this.props || {}, thisState = this.state || {};
+
+        // if (Object.keys(thisProps).length !== Object.keys(nextProps).length ||
+        //     Object.keys(thisState).length !== Object.keys(nextState).length) {
+        //     return true;
+        // }
+
+        // for (const key in nextProps) {
+        //     if (!is(thisProps[key], nextProps[key])) {
+        //         return true;
+        //     }
+        // }
+
+        // for (const key in nextState) {
+        //     if (thisState[key] !== nextState[key] || !is(thisState[key], nextState[key])) {
+        //         return true;
+        //     }
+        // }
+        // return false;
+    }
+
+
+    render(){
+        console.log(1111);
+        return (
+            <div>
+                1
+            </div>
+        )
+    }
+}
 
 
 export default Test;
