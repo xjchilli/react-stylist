5fec5efa28c4d7a189d2e3fdc0b15f7f99fc2e69
potato1314 <wangzehu1314@sina.cn>
2017-09-06 18:29:54 +0800

    代码优化

--- serverDev.js
+++ serverDev.js
@@ -5,18 +5,18 @@ let proxy = require('http-proxy-middleware');
 let winston = require('winston');
 let https = require('https');
 let fs = require('fs');
+let compress = require('compression');
 
 
+app.use(compress());//开启gzip
 app.use(express.static(__dirname));
 
-https.createServer({
-	// key: fs.readFileSync('/usr/local/nginx/conf/cert/2_wx.dapeis.net.key'),
-	// cert: fs.readFileSync('/usr/local/nginx/conf/cert/1_wx.dapeis.net_bundle.crt')
-	key: fs.readFileSync('./Nginx/2_wx.dapeis.net.key'),
-	cert: fs.readFileSync('./Nginx/1_wx.dapeis.net_bundle.crt')
-}, app).listen(port);
+// https.createServer({
+// 	key: fs.readFileSync('/usr/local/nginx/conf/cert/2_wx.dapeis.net.key'),
+// 	cert: fs.readFileSync('/usr/local/nginx/conf/cert/1_wx.dapeis.net_bundle.crt')
+// }, app).listen(port);
 
-// app.listen(port);
+app.listen(port);
 
 console.log('started on port ' + port);
 
