13b2159763e698ca2fe20500f82b5b88ad52e6ab
xiaowang1314 <wangzehu1314@sina.cn>
2017-11-28 21:16:09 +0800

    change

--- src/Component/preview/previewImg.js
+++ src/Component/preview/previewImg.js
@@ -11,15 +11,58 @@
 import React from 'react';
 
 
-class PreviewImg extends React.Component {   
+class PreviewImg extends React.Component {
+
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         num: 0,
+    //         isMove: 0,
+    //         isEnd: 0
+    //     }
+    //     this.move = this.move.bind(this);
+    // }
+
+    // componentDidMount() {
+    //     this.imgEle.addEventListener('touchstart', (e) => {
+    //         this.setState({
+    //             num: e.touches.length
+    //         });
+    //         console.log(e.touches.length);
+
+    //         this.imgEle.addEventListener('touchmove', this.move);//move 
+    //     });
+
+
+
+    //     this.imgEle.addEventListener('touchend', (e) => {
+    //         this.imgEle.removeEventListener('touchmove', this.move);
+    //         this.setState({
+    //             isEnd: Math.random() * 100
+    //         });
+    //     });
+
+
+    // }
+
+    // move() {
+    //     this.setState({
+    //         isMove: Math.random() * 100
+    //     });
+    // }
+
+
     hide(event) {
         event.preventDefault();
         this.props.hidePreviewBigImg();
     }
     render() {
         return (
-            <div className="preview-container" onClick={this.hide.bind(this)} onTouchStart={(e)=>{e.preventDefault()}}>
-                <img src={this.props.url} alt="" />
+            <div className="preview-container" onClick={this.hide.bind(this)} onTouchStart={(e) => { e.preventDefault() }}>
+                {/* <p style={{ color: 'red' }}>{this.state.num}</p>
+                <p style={{ color: 'red' }}>isMove:{this.state.isMove}</p>
+                <p style={{ color: 'red' }}>isMove:{this.state.isEnd}</p> */}
+                <img src={this.props.url} alt="" ref={(el) => this.imgEle = el} />
             </div>
         )
     }
